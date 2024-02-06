5baf7cf3ae08791632fee578eb9e3e60f668fbf7
Leon Miftari <148079413+LeonMiftari@users.noreply.github.com>
2024-02-06 16:27:41 +0100

    Added React Router DOM for navigation and created new pages: Home, Layout, Pricing, OurCards, Customers, Contact, SignUp, Login, and NotFound.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
 import Sidebar from "./Organisms/DashboardSidebar/Sidebar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Layout from './Pages/Layout';
+import Pricing from './Pages/Pricing';
+import OurCards from './Pages/OurCards';
+import Customers from './Pages/Customers';
+import Contact from './Pages/Contact';
+import SignUp from './Pages/SignUp';
+import Login from './Pages/Login';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
-    <>
+    <div>
       <Sidebar />
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/ourcards" element={<OurCards />} />
+
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/Pages/OurCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OurCards() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OurCards
