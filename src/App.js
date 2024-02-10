71b1002369cd78b6496855d7dab73999dd2da26e
SaimirBunjaku <saimirbunjaku@gmail.com>
2024-02-10 09:54:14 +0100

    Added Pages and Routes. Note: The pages are not the final links to be routed to until furthermore discussed with the team.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Organisms/Navbar";
+import Home from "./Pages/Home";
+import Pricing from "./Pages/Pricing";
+import OurCards from "./Pages/OurCards";
+import Customers from "./Pages/Customers";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/cards" element={<OurCards />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ src/Pages/OurCards.js
@@ -0,0 +1,5 @@
+function OurCards() {
+  return <div>Our Cards Page</div>;
+}
+
+export default OurCards;
