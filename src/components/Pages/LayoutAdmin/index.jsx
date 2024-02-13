dcca7138069d018ca7ddbaaeee8162211943b25b
Lisa-Ramizi <lisaramizi1@gmail.com>
2024-02-13 17:06:50 +0100

    added a responsive header card for the admin page

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AdminHeaderComponent from '../../molecules/AdminHeaderComponent/index'
+import Sidebar from '../../molecules/Sidebar'
 
 const AdminLayout = () => {
   return (
     <div className='adminLayout'>
       <div>AdminLayout</div>
+      <AdminHeaderComponent />
     </div>
   )
 }
