0a2fcd77042012c81c372bdd058bbf9ed6e038b4
Shpat Spahiu <shpat.spahiu@proton.me>
2024-02-13 16:34:18 +0100

    Improve Modal functionality
    Add a backdrop, close with esc key, close on click outside

--- src/components/Pages/LayoutClient/index.jsx
+++ src/components/Pages/LayoutClient/index.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../molecules/Navbar'
+import ModalBasic from '../../molecules/ModalBasic'
 
 const ClientLayout = () => {
+  const [modalOpen, setModalOpen] = useState(false)
   return (
     <>
       <div>
+        <button
+          onClick={() => {
+            setModalOpen(true)
+          }}
+        >
+          Open modal
+        </button>
         <Navbar />
+        {modalOpen && (
+          <ModalBasic
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+            title={'Hello Wolrd'}
+          >
+            Hello World bla bla bla Hello World bla bla bla Hello World bla bla
+            bla Hello World bla bla bla Hello World bla bla bla Hello World bla
+            bla bla Hello World bla bla bla Hello World bla bla bla
+          </ModalBasic>
+        )}
         <Outlet />
       </div>
     </>
---
+++ src/components/molecules/ModalBasic/index.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import './ModalBasic.scss'
+import BackDrop from '../../atoms/BackDrop'
+
+const ModalBasic = ({ children, title, modalOpen, setModalOpen }) => {
+
+  // close if ESC pressed
+  useEffect(() => {
+    const keyHandler = ({ keyCode }) => {
+      if (!modalOpen || keyCode !== 27) return
+      setModalOpen(false)
+    }
+    document.addEventListener('keydown', keyHandler)
+    return () => document.removeEventListener('keydown', keyHandler)
+  })
+
+  return (
+    <>
+      <BackDrop setModalOpen={setModalOpen} />
+      <div className='modalBackground'>
+        <div className='modalContainer'>
+          <div className='header'>
+            <div className='title'>{title}</div>
+            <div
+              onClick={() => {
+                setModalOpen(false)
+              }}
+              className='closeBtn'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className=''
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                strokeWidth='2.5'
+                stroke='currentColor'
+                fill='none'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M18 6l-12 12' />
+                <path d='M6 6l12 12' />
+              </svg>
+            </div>
+          </div>
+          <div className='body'>{children}</div>
+          <div className='footer'>
+            <button onClick={() => setModalOpen(false)}>Close</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ModalBasic
