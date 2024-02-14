fa29dd871b866b7b493f4b1bddac03119cb0cde8
Shpat Spahiu <shpat.spahiu@proton.me>
2024-02-14 11:42:42 +0100

    Resolve confilts in AdminLayout

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ModalDemo from '../../organisms/ModalDemo'
+import AdminHeaderComponent from '../../molecules/AdminHeaderComponent/index'
 
 const AdminLayout = () => {
   return (
     <div className='adminLayout'>
       <div>AdminLayout</div>
       <ModalDemo />
+      <AdminHeaderComponent />
     </div>
   )
 }
