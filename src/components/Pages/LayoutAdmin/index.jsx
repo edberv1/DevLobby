bc03e631e643cc99d9d97cfb6eb0fa94d0601689
Lisa-Ramizi <lisaramizi1@gmail.com>
2024-02-15 17:21:51 +0100

    created the big chart feature for the admin page

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import AdminHeaderComponent from '../../molecules/AdminHeaderComponent/index'
 import Sidebar from '../../molecules/Sidebar'
+import BigChartAndStats from '../../organisms/BigChartAndStats'
+
 
 const AdminLayout = () => {
   return (
     <div className='adminLayout'>
       <div>AdminLayout</div>
       <AdminHeaderComponent />
+      <BigChartAndStats />
     </div>
   )
 }
