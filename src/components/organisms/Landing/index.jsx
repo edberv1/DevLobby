4aaad18ba4d7fd87ba2573e6ec4dfe940593776d
SaimirBunjaku <saimirbunjaku@gmail.com>
2024-02-16 13:45:07 +0100

    Content about Chat, commiting now to pull the new stuff from dev

--- src/components/organisms/Landing/index.jsx
+++ src/components/organisms/Landing/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ClientHeaderSection from '../../molecules/ClientHeaderSection'
-import Cards from '../Cards'
+import React from "react";
+import ClientHeaderSection from "../../molecules/ClientHeaderSection";
+import Cards from "../Cards";
+import ContentModule from "../../molecules/ContentModule";
 
 const Landing = () => {
   return (
     <div>
       <ClientHeaderSection />
       <Cards />
+      <ContentModule />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
