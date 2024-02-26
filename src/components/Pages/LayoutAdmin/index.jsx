940b81ae569cfe0fbfd2b8499fc4ea2777f25d30
Shpat Spahiu <shpat.spahiu@proton.me>
2024-02-26 10:45:55 +0100

    Create AuthContext.js

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -12,7 +12,6 @@ const AdminLayout = () => {
   return (
     <div className='adminLayout'>
       <Sidebar/>
-      <ModalDemo />
       <AdminHeaderComponent />
       <BigChartAndStats />
       <SmallChartsCombined />
