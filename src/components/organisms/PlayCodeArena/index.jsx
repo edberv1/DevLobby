530e44f9c9b264ca983b0f0dfe5bda3676380723
Lisa-Ramizi <lisaramizi1@gmail.com>
2024-02-26 17:01:37 +0100

    Created functional cards for the play code arena, fully responive

--- src/components/organisms/PlayCodeArena/index.jsx
+++ src/components/organisms/PlayCodeArena/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import PlayArenaCards from '../../molecules/PlayArenaCards'
 import PlayArenaHeading from '../../atoms/PlayArenaHeading'
 
@@ -7,8 +8,13 @@ const PlayCodeArena = () => {
     <div>
       <PlayArenaHeading />
       <PlayArenaCards />
+      <Link to="/polls">
+        Polls Page
+      </Link>
+      <h1>PlayCode Arena Page</h1>
+      <p>Welcome to the PlayCode Arena page!</p>
     </div>
-  )
+  );
 }
 
-export default PlayCodeArena
+export default PlayCodeArena;
