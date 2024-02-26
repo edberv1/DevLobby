866b784c5d75519d30f8492b316bca4c1f6235b8
Shpat Spahiu <shpat.spahiu@proton.me>
2024-02-26 16:27:44 +0100

    Add background image to PageNotFound

--- src/components/organisms/PageNotFound/index.jsx
+++ src/components/organisms/PageNotFound/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './PageNotFound.scss'
+import Void from '../../../assets/images/void.svg'
 
 const PageNotFound = () => {
   return (
@@ -8,11 +9,14 @@ const PageNotFound = () => {
       <div className='container'>
         <div id='errCode'>404 not found :(</div>
         <div className='text2'>The page you're looking for doesn't exist.</div>
-      </div>
-      <div className='guide'>
-        <div className='homeBtn'>
-          <Link to='/'>Go back to Lobby</Link>
+        <div className='guide'>
+          <Link to='/'>
+            <div className='homeBtn'>Go back to Lobby</div>
+          </Link>
         </div>
+      </div>
+      <div className='imgContainer'>
+        <img src={Void} alt='lost' />
       </div>
     </div>
   )
