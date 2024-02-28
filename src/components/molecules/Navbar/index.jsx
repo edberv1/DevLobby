08bfc58b316fcecce4aec8c307e8e4211107f0a5
SaimirBunjaku <saimirbunjaku@gmail.com>
2024-02-28 10:02:30 +0100

    New dev

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.scss";
 import devLobbylogo from "../../../assets/images/devlobby-logo-cut.png";
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUsername = localStorage.getItem("username");
+    if (token && storedUsername) {
+      setIsLoggedIn(true);
+      setUsername(storedUsername);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -14,15 +28,21 @@ const Navbar = () => {
     setMenuOpen(false);
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
+
   return (
     <>
       <nav className={`navbar ${isMenuOpen ? "menu-open" : ""}`}>
         <Link to="/">
           <div className="navbar-logo">
-            <img src={devLobbylogo} alt="DevLoby Logo" onClick={closeMenu} />
+            <img src={devLobbylogo} alt="DevLobby Logo" onClick={closeMenu} />
           </div>
         </Link>
-        {/* Hamburger menu icon */}
         <div
           className="hamburger-menu"
           onClick={toggleMenu}
@@ -33,8 +53,8 @@ const Navbar = () => {
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
-        {/* Navbar links */}
         <ul className={`navbar-links ${isMenuOpen ? "menu-open" : ""}`}>
+          {/* Your existing navbar links */}
           <li onClick={closeMenu}>
             <Link to="/" className="home">
               Home
@@ -57,28 +77,60 @@ const Navbar = () => {
           <li onClick={closeMenu}>
             <Link to="/contactus">Contact Us</Link>
           </li>
-          <div>
-            <button className="butonat-nav login" onClick={closeMenu}>
-              <Link to="/login">Log in</Link>
-            </button>
-            <button className="butonat-nav signup" onClick={closeMenu}>
-              <Link to="/signup">Sign Up</Link>
-            </button>
+          {/* Conditionally render user info and sign out if logged in */}
+          <div className="butonat-nav">
+            {isLoggedIn ? (
+              <li onClick={handleLogout}>{username} | Sign Out</li>
+            ) : (
+              <>
+                <li>
+                  <button className="butonat-nav login" onClick={closeMenu}>
+                    <Link to="/login">Log in</Link>
+                  </button>
+                </li>
+                <li>
+                  <button className="butonat-nav signup" onClick={closeMenu}>
+                    <Link to="/signup">Sign Up</Link>
+                  </button>
+                </li>
+              </>
+            )}
           </div>
         </ul>
-        {/* Buttons */}
+
+        {/* Conditionally render login/signup buttons or user info in the client buttons section */}
         <div className="butonat-client">
-          <button className="login">
-            <Link to="/login">Log in</Link>
-          </button>
-          <button className="signup" onClick={closeMenu}>
-            <Link to="/signup">Sign Up</Link>
-          </button>
+          {isLoggedIn ? (
+            <>
+              <span className="navbar-username" style={{ color: "white" }}>
+                {username}
+              </span>
+              <button
+                onClick={handleLogout}
+                className="navbar-logout"
+                style={{
+                  backgroundColor: "#0070ec",
+                  color: "white",
+                }}
+              >
+                Sign Out
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="login" onClick={closeMenu}>
+                <Link to="/login">Log in</Link>
+              </button>
+              <button className="signup" onClick={closeMenu}>
+                <Link to="/signup">Sign Up</Link>
+              </button>
+            </>
+          )}
           <button className="profile" onClick={closeMenu}>
-          <Link to="/profile">
-            <i className="fas fa-user-circle fa-2x"></i>
-          </Link>
-        </button>
+            <Link to="/profile">
+              <i className="fas fa-user-circle fa-2x"></i>
+            </Link>
+          </button>
         </div>
       </nav>
     </>
