d12aa08e6cbd4dd7148441a1c5076e977e3ba891
Shpat Spahiu <shpat.spahiu@proton.me>
2024-02-29 17:50:27 +0100

    Add ModalDemo and DashboardCard to AdminLayout

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -6,7 +6,9 @@ import Sidebar from '../../molecules/Sidebar'
 import BigChartAndStats from '../../organisms/BigChartAndStats'
 import SmallChartsCombined from '../../organisms/SmallChartsCombined'
 import { Navigate } from 'react-router-dom'
-import UserTable from "../../molecules/UserTable"
+import UserTable from '../../molecules/UserTable'
+import DashboardCard from '../../molecules/DashboardCard'
+import ModalDemo from '../../organisms/ModalDemo'
 
 const AdminLayout = () => {
   const { isLoggedIn, logout } = useContext(AuthContext)
@@ -15,11 +17,13 @@ const AdminLayout = () => {
     <>
       {isLoggedIn ? (
         <div className='adminLayout'>
-          <button onClick={logout}>Logout</button>
           <Sidebar />
+          <button onClick={logout}>Logout</button>
+          <ModalDemo />
           <AdminHeaderComponent />
           <BigChartAndStats />
           <SmallChartsCombined />
+          <DashboardCard title={'Generic card'} />
           <UserTable></UserTable>
         </div>
       ) : (
