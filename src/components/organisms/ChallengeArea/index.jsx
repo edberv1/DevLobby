cccf6dd3fc5b7cdd0e094e1017ae1b626a545555
SaimirBunjaku <saimirbunjaku@gmail.com>
2024-03-01 15:18:42 +0100

    Fixed duplicate footer

--- src/components/organisms/ChallengeArea/index.jsx
+++ src/components/organisms/ChallengeArea/index.jsx
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import ChallengeTable from "../../atoms/ChallengeTable";
 import ChallengeButtons from "../../molecules/ChallengeButtons";
-import './ChallengeArea.scss';
+import "./ChallengeArea.scss";
 import { data } from "./data";
-import ChallengeHeader from '../../atoms/ChallengeHeader';
-import Footer from '../Footer';
-import Navbar from '../../molecules/Navbar'
+import ChallengeHeader from "../../atoms/ChallengeHeader";
+import Footer from "../Footer";
+import Navbar from "../../molecules/Navbar";
 
 export default function ChallengeArea() {
-    const [sortCategory, setSortCategory] = useState('');
+  const [sortCategory, setSortCategory] = useState("");
 
-    const handleClick = (category) => {
-        setSortCategory(category);
-    };
+  const handleClick = (category) => {
+    setSortCategory(category);
+  };
 
-    const handleSortChange = ({ column, order }) => {
-        return { column, order };
-    };
+  const handleSortChange = ({ column, order }) => {
+    return { column, order };
+  };
 
-    return (
-        <div className="challenge-tab">
-            <div className="table-button">
-                <ChallengeHeader />
-                <ChallengeButtons handleClick={handleClick} activeCategory={sortCategory} />
-                <ChallengeTable challenges={data} sortCategory={sortCategory} onSortChange={handleSortChange} />
-                <Footer/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="challenge-tab">
+      <div className="table-button">
+        <ChallengeHeader />
+        <ChallengeButtons
+          handleClick={handleClick}
+          activeCategory={sortCategory}
+        />
+        <ChallengeTable
+          challenges={data}
+          sortCategory={sortCategory}
+          onSortChange={handleSortChange}
+        />
+      </div>
+    </div>
+  );
 }
-
-
-
