521fcba8c7b2ed886ba39785d63dbadbbd10508f
Rrezon Curraj <rrezoncurraj10@gmail.com>
2024-03-05 11:14:14 +0100

    slowed the animation of testimonials

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -1,36 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 
-import { AuthContext } from '../../../utils/AuthContext'
-import AdminHeaderComponent from '../../molecules/AdminHeaderComponent/index'
-import Sidebar from '../../molecules/Sidebar'
-import BigChartAndStats from '../../organisms/BigChartAndStats'
-import SmallChartsCombined from '../../organisms/SmallChartsCombined'
-import { Navigate } from 'react-router-dom'
-import UserTable from '../../molecules/UserTable'
-import DashboardCard from '../../molecules/DashboardCard'
-import ModalDemo from '../../organisms/ModalDemo'
+import { AuthContext } from "../../../utils/AuthContext";
+import AdminHeaderComponent from "../../molecules/AdminHeaderComponent/index";
+import Sidebar from "../../molecules/Sidebar";
+import BigChartAndStats from "../../organisms/BigChartAndStats";
+import SmallChartsCombined from "../../organisms/SmallChartsCombined";
+import { Navigate } from "react-router-dom";
+import UserTable from "../../molecules/UserTable";
+import DashboardCard from "../../molecules/DashboardCard";
+import ModalDemo from "../../organisms/ModalDemo";
 
 const AdminLayout = () => {
-  const { isLoggedIn, logout } = useContext(AuthContext)
-
+  const { isLoggedIn, logout } = useContext(AuthContext);
   return (
     <>
       {isLoggedIn ? (
-        <div className='adminLayout'>
+        <div className="adminLayout">
           <Sidebar />
           <button onClick={logout}>Logout</button>
           <ModalDemo />
           <AdminHeaderComponent />
           <BigChartAndStats />
           <SmallChartsCombined />
-          <DashboardCard title={'Generic card'} />
+          <DashboardCard title={"Generic card"} />
           <UserTable></UserTable>
         </div>
       ) : (
-        <Navigate to='/login' />
+        <Navigate to="/login" />
       )}
     </>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
