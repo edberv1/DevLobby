108b5c121e32fed421d85fd61a85aaf46a35cc28
Lisa-Ramizi <lisaramizi1@gmail.com>
2024-03-06 09:50:30 +0100

    created responsive header ribbon for the admin page

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -1,23 +1,27 @@
 import React, { useContext } from "react";
 
-import { AuthContext } from "../../../utils/AuthContext";
-import AdminHeaderComponent from "../../molecules/AdminHeaderComponent/index";
-import Sidebar from "../../molecules/Sidebar";
-import BigChartAndStats from "../../organisms/BigChartAndStats";
-import SmallChartsCombined from "../../organisms/SmallChartsCombined";
-import { Navigate } from "react-router-dom";
-import UserTable from "../../molecules/UserTable";
-import DashboardCard from "../../molecules/DashboardCard";
-import ModalDemo from "../../organisms/ModalDemo";
+import { AuthContext } from '../../../utils/AuthContext'
+import AdminHeaderComponent from '../../molecules/AdminHeaderComponent/index'
+import Sidebar from '../../molecules/Sidebar'
+import BigChartAndStats from '../../organisms/BigChartAndStats'
+import SmallChartsCombined from '../../organisms/SmallChartsCombined'
+import { Navigate } from 'react-router-dom'
+import UserTable from '../../molecules/UserTable'
+import DashboardCard from '../../molecules/DashboardCard'
+import ModalDemo from '../../organisms/ModalDemo'
+import DashboardHeader from '../../organisms/DashboardHeader'
+import "./LayoutAdmin.scss"
 
 const AdminLayout = () => {
-  const { isLoggedIn, logout } = useContext(AuthContext);
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <>
       {isLoggedIn ? (
-        <div className="adminLayout">
-          <Sidebar />
-          <button onClick={logout}>Logout</button>
+        <div className='adminLayout'>
+          <div className="sidebar-header-container">
+            <Sidebar />
+            <DashboardHeader />
+          </div>
           <ModalDemo />
           <AdminHeaderComponent />
           <BigChartAndStats />
