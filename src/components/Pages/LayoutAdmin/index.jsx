eea6e9f6fa05862641ba3b90384d3ed431e6ef77
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-12 23:13:56 +0100

    Remove Chart components from AdminLayout

--- src/components/Pages/LayoutAdmin/index.jsx
+++ src/components/Pages/LayoutAdmin/index.jsx
@@ -1,35 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react'
 
-import { AuthContext } from "../../../utils/AuthContext";
-import AdminHeaderComponent from "../../molecules/AdminHeaderComponent/index";
-import Sidebar from "../../molecules/Sidebar";
-import BigChartAndStats from "../../organisms/BigChartAndStats";
-import SmallChartsCombined from "../../organisms/SmallChartsCombined";
-import { Navigate } from "react-router-dom";
-import UserTable from "../../molecules/UserTable";
-import ModalDemo from "../../organisms/ModalDemo";
-import DashboardCardAndTaskManager from "../../organisms/DashboardCardAndTaskManager"
+import { AuthContext } from '../../../utils/AuthContext'
+import AdminHeaderComponent from '../../molecules/AdminHeaderComponent/index'
+import Sidebar from '../../molecules/Sidebar'
+import BigChartAndStats from '../../organisms/BigChartAndStats'
+import SmallChartsCombined from '../../organisms/SmallChartsCombined'
+import { Navigate } from 'react-router-dom'
+import UserTable from '../../molecules/UserTable'
+import ModalDemo from '../../organisms/ModalDemo'
+import DashboardCardAndTaskManager from '../../organisms/DashboardCardAndTaskManager'
 
 const AdminLayout = () => {
-  const { isLoggedIn, logout } = useContext(AuthContext);
+  const { isLoggedIn, logout } = useContext(AuthContext)
   return (
     <>
       {isLoggedIn ? (
-        <div className="adminLayout">
+        <div className='adminLayout'>
           <Sidebar />
           <button onClick={logout}>Logout</button>
           <ModalDemo />
           <AdminHeaderComponent />
-          <BigChartAndStats />
-          <SmallChartsCombined />
+          {/* <BigChartAndStats /> */}
+          {/* <SmallChartsCombined /> */}
           <DashboardCardAndTaskManager />
           <UserTable></UserTable>
         </div>
       ) : (
-        <Navigate to="/login" />
+        <Navigate to='/login' />
       )}
     </>
-  );
-};
+  )
+}
 
-export default AdminLayout;
+export default AdminLayout
