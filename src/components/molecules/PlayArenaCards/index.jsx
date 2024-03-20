71f33bc8a053ca2365a285e03b7a6c245ae8f7e7
Lisa-Ramizi <lisaramizi1@gmail.com>
2024-03-20 13:24:36 +0100

    Refactor card selection to use strings instead of numbers

--- src/components/molecules/PlayArenaCards/index.jsx
+++ src/components/molecules/PlayArenaCards/index.jsx
@@ -8,34 +8,33 @@ import ChallengeArea from '../../organisms/ChallengeArea';
 function PlayArenaCards() {
   const [selectedCard, setSelectedCard] = useState(null);
 
-  const handleCardSelect = (cardIndex) => {
-    setSelectedCard(cardIndex);
+  const handleCardSelect = (cardName) => {
+    setSelectedCard(cardName);
   };
 
   return (
     <>
-    <div className="play-arena-cards-container">
-      <div className="play-arena-cards">
-        <div className={`card ${selectedCard === 0 ? 'selected' : ''}`}>
-          <div className="content">
-            <h2>Theoretical Questions</h2>
-            <p>Test your theory knowledge and expertise with a variety of theoretical questions. Explore concepts, principles, and frameworks to expand your understanding.</p>
+      <div className="play-arena-cards-container">
+        <div className="play-arena-cards">
+          <div className={`card ${selectedCard === 'Theory' ? 'selected' : ''}`}>
+            <div className="content">
+              <h2>Theoretical Questions</h2>
+              <p>Test your theory knowledge and expertise with a variety of theoretical questions. Explore concepts, principles, and frameworks to expand your understanding.</p>
+            </div>
+            <div className="image" style={{ backgroundImage: `url(${theoryImage})` }}></div>
+            <SelectButton onClick={() => handleCardSelect('Theory')} selected={selectedCard === 'Theory'} />
           </div>
-          <div className="image" style={{ backgroundImage: `url(${theoryImage})` }}></div>
-          <SelectButton onClick={() => handleCardSelect(0)} selected={selectedCard === 0} />
-        </div>
-        <div className={`card ${selectedCard === 1 ? 'selected' : ''}`}>
-          <div className="content">
-            <h2>Coding Questions</h2>
-            <p>Practice your coding skills with challenging coding questions. Solve problems, write algorithms, and improve your programming proficiency.</p>
+          <div className={`card ${selectedCard === 'Code' ? 'selected' : ''}`}>
+            <div className="content">
+              <h2>Coding Questions</h2>
+              <p>Practice your coding skills with challenging coding questions. Solve problems, write algorithms, and improve your programming proficiency.</p>
+            </div>
+            <div className="image" style={{ backgroundImage: `url(${practiceImage})` }}></div>
+            <SelectButton onClick={() => handleCardSelect('Code')} selected={selectedCard === 'Code'} />
           </div>
-          <div className="image" style={{ backgroundImage: `url(${practiceImage})` }}></div>
-          <SelectButton onClick={() => handleCardSelect(1)} selected={selectedCard === 1} />
-          
         </div>
+        {selectedCard === 'Code' && <ChallengeArea />}
       </div>
-      {selectedCard === 1 && <ChallengeArea />}
-    </div>
     </>
   );
 }
