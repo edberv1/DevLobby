9b068f37c0069cb8af3b4fd4857a93cb1cca850a
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-22 16:01:58 +0100

    Put in navbar, functionality improvements

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -1,100 +1,101 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.scss";
-import devLobbylogo from "../../../assets/images/devlobby-logo-cut.png";
-import { AuthContext } from "../../../utils/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.scss'
+import devLobbylogo from '../../../assets/images/devlobby-logo-cut.png'
+import { AuthContext } from '../../../utils/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import NotificationCenter from '../NotificationCenter'
 
 const Navbar = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const { isLoggedIn, logout } = useContext(AuthContext);
+  const [isMenuOpen, setMenuOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024)
+  const [showDropdown, setShowDropdown] = useState(false)
+  const { isLoggedIn, logout } = useContext(AuthContext)
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const handleLoginClick = () => {
-    navigate("/login");
-  };
+    navigate('/login')
+  }
 
   const handleSignupClick = () => {
-    navigate("/signup");
-  };
+    navigate('/signup')
+  }
 
-  const handleCloseMenuAndRedirect = (redirectFunction) => {
+  const handleCloseMenuAndRedirect = redirectFunction => {
     return () => {
-      closeMenu();
-      redirectFunction();
-    };
-  };
+      closeMenu()
+      redirectFunction()
+    }
+  }
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1024);
-    };
+      setIsMobile(window.innerWidth <= 1024)
+    }
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
-    const handleOutsideClick = (event) => {
-      if (showDropdown && !event.target.closest(".user-greeting")) {
-        setShowDropdown(false);
+    const handleOutsideClick = event => {
+      if (showDropdown && !event.target.closest('.user-greeting')) {
+        setShowDropdown(false)
       }
-    };
+    }
 
-    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener('mousedown', handleOutsideClick)
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, [showDropdown]);
+      window.removeEventListener('resize', handleResize)
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [showDropdown])
 
   const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+    setMenuOpen(!isMenuOpen)
+  }
 
   const closeMenu = () => {
-    setMenuOpen(false);
-    setShowDropdown(false);
-  };
+    setMenuOpen(false)
+    setShowDropdown(false)
+  }
 
   const handleLogout = () => {
-    logout();
-    closeMenu();
-    navigate("/");
-  };
+    logout()
+    closeMenu()
+    navigate('/')
+  }
 
   const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
+    setShowDropdown(!showDropdown)
+  }
 
   return (
-    <nav className={`navbar ${isMenuOpen ? "menu-open" : ""}`}>
+    <nav className={`navbar ${isMenuOpen ? 'menu-open' : ''}`}>
       {(!isMenuOpen || !isMobile || !isLoggedIn) && (
-        <Link to="/">
-          <div className="navbar-logo">
-            <img src={devLobbylogo} alt="DevLobby Logo" onClick={closeMenu} />
+        <Link to='/'>
+          <div className='navbar-logo'>
+            <img src={devLobbylogo} alt='DevLobby Logo' onClick={closeMenu} />
           </div>
         </Link>
       )}
       {isLoggedIn && isMenuOpen && isMobile && (
         <button
-          className="navbar-profile-icon user-greeting"
+          className='navbar-profile-icon user-greeting'
           onClick={toggleDropdown}
         >
-          <i className="fas fa-user-circle fa-2x"></i>
+          <i className='fas fa-user-circle fa-2x'></i>
           {showDropdown && (
             <div
               className={`dropdown-menu ${
-                showDropdown ? "dropdown-menu-active" : ""
+                showDropdown ? 'dropdown-menu-active' : ''
               }`}
             >
               <Link
-                to="/profile"
+                to='/profile'
                 onClick={closeMenu}
-                style={{ borderBottom: "1px solid gray", borderRadius: 0 }}
+                style={{ borderBottom: '1px solid gray', borderRadius: 0 }}
               >
                 Profile
               </Link>
-              <Link to="/settings" onClick={closeMenu}>
+              <Link to='/settings' onClick={closeMenu}>
                 Settings
               </Link>
             </div>
@@ -103,69 +104,69 @@ const Navbar = () => {
       )}
 
       <div
-        className="hamburger-menu"
+        className='hamburger-menu'
         onClick={toggleMenu}
-        role="button"
-        tabIndex="0"
+        role='button'
+        tabIndex='0'
       >
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+        <div className='bar'></div>
+        <div className='bar'></div>
+        <div className='bar'></div>
       </div>
-      <ul className={`navbar-links ${isMenuOpen ? "menu-open" : ""}`}>
+      <ul className={`navbar-links ${isMenuOpen ? 'menu-open' : ''}`}>
         <li onClick={closeMenu}>
-          <Link to="/" className="home">
+          <Link to='/' className='home'>
             Home
           </Link>
         </li>
         <li onClick={closeMenu}>
-          <Link to="/playcodearena" className="playcodearena">
+          <Link to='/playcodearena' className='playcodearena'>
             PlayCode Arena
           </Link>
         </li>
         {isLoggedIn && (
           <li onClick={closeMenu}>
-            <Link to="/chat">Chat</Link>
+            <Link to='/chat'>Chat</Link>
           </li>
         )}
         <li onClick={closeMenu}>
-          <Link to="/blog">Blog</Link>
+          <Link to='/blog'>Blog</Link>
         </li>
         <li onClick={closeMenu}>
-          <Link to="/about">About</Link>
+          <Link to='/about'>About</Link>
         </li>
         <li onClick={closeMenu}>
-          <Link to="/contactus">Contact Us</Link>
+          <Link to='/contactus'>Contact Us</Link>
         </li>
         {!isLoggedIn && (
           <div
-            className={`buttons-container ${isMenuOpen ? "menu-active" : ""}`}
+            className={`buttons-container ${isMenuOpen ? 'menu-active' : ''}`}
           >
-            <button className="butonat-nav login" onClick={closeMenu}>
-              <Link to="/login">Log in</Link>
+            <button className='butonat-nav login' onClick={closeMenu}>
+              <Link to='/login'>Log in</Link>
             </button>
-            <button className="butonat-nav signup" onClick={closeMenu}>
-              <Link to="/signup">Sign Up</Link>
+            <button className='butonat-nav signup' onClick={closeMenu}>
+              <Link to='/signup'>Sign Up</Link>
             </button>
           </div>
         )}
         {isLoggedIn && (
-          <li className="navbar-logout" onClick={handleLogout}>
+          <li className='navbar-logout' onClick={handleLogout}>
             Sign Out
           </li>
         )}
       </ul>
-      <div className="butonat-client">
+      <div className='butonat-client'>
         {!isLoggedIn && (
           <>
             <button
-              className="login"
+              className='login'
               onClick={handleCloseMenuAndRedirect(handleLoginClick)}
             >
               Log in
             </button>
             <button
-              className="signup"
+              className='signup'
               onClick={handleCloseMenuAndRedirect(handleSignupClick)}
             >
               Sign Up
@@ -173,32 +174,35 @@ const Navbar = () => {
           </>
         )}
         {isLoggedIn && (
-          <div
-            className={`user-greeting ${showDropdown ? "showDropdown" : ""}`}
-          >
-            <button onClick={toggleDropdown} className="profile">
-              <i className="fas fa-user-circle fa-2x"></i>
-            </button>
-            {showDropdown && (
-              <div
-                className={`dropdown-menu ${
-                  showDropdown ? "dropdown-menu-active" : ""
-                }`}
-              >
-                <Link to="/profile" onClick={closeMenu}>
-                  Profile
-                </Link>
-                <Link to="/settings" onClick={closeMenu}>
-                  Settings
-                </Link>
-                <button onClick={handleLogout}>Log out</button>
-              </div>
-            )}
-          </div>
+          <>
+            <NotificationCenter />
+            <div
+              className={`user-greeting ${showDropdown ? 'showDropdown' : ''}`}
+            >
+              <button onClick={toggleDropdown} className='profile'>
+                <i className='fas fa-user-circle fa-2x'></i>
+              </button>
+              {showDropdown && (
+                <div
+                  className={`dropdown-menu ${
+                    showDropdown ? 'dropdown-menu-active' : ''
+                  }`}
+                >
+                  <Link to='/profile' onClick={closeMenu}>
+                    Profile
+                  </Link>
+                  <Link to='/settings' onClick={closeMenu}>
+                    Settings
+                  </Link>
+                  <button onClick={handleLogout}>Log out</button>
+                </div>
+              )}
+            </div>
+          </>
         )}
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
