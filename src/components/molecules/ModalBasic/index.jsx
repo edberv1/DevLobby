eef15d2e1bc191f50db6b5ca7a03d1c87361806f
Edber Vuciterna <vuciternaedber@gmail.com>
2024-03-28 01:41:16 +0100

    Edit functionality

--- src/components/molecules/ModalBasic/index.jsx
+++ src/components/molecules/ModalBasic/index.jsx
@@ -33,39 +33,39 @@ const ModalBasic = ({ children, title, modalOpen, setModalOpen }) => {
   return (
     <>
       {modalOpen && (
-        <div className='modalBasic'>
+        <div className="modalBasic">
           <BackDrop setModalOpen={setModalOpen} />
           <div className={`modalContainer${renderModal ? ' show' : ''}`}>
-            <div className='modalHeader'>
-              <div className='title'>{title}</div>
+            <div className="modalHeader">
+              <div className="title">{title}</div>
               <div
                 onClick={() => {
                   setModalOpen(false)
                 }}
-                className='closeBtn'
+                className="closeBtn"
               >
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className=''
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  strokeWidth='2.5'
-                  stroke='currentColor'
-                  fill='none'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
+                  xmlns="http://www.w3.org/2000/svg"
+                  className=""
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-                  <path d='M18 6l-12 12' />
-                  <path d='M6 6l12 12' />
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M18 6l-12 12" />
+                  <path d="M6 6l12 12" />
                 </svg>
               </div>
             </div>
-            <div className='body'>{children}</div>
-            <div className='modalFooter'>
+            <div className="body">{children}</div>
+            {/* <div className='modalFooter'>
               <button onClick={() => setModalOpen(false)}>Close</button>
-            </div>
+            </div> */}
           </div>
         </div>
       )}
--- src/components/molecules/UserTable/index.jsx
+++ src/components/molecules/UserTable/index.jsx
@@ -1,102 +1,150 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 import {
   FaSortAmountUp,
   FaSortAmountDown,
   FaEdit,
   FaSearch,
-} from "react-icons/fa";
-import { MdDeleteForever } from "react-icons/md";
-import "./UserTable.scss";
-import ModalDelete from "../ModalDelete/index";
-import Pagination from "../../atoms/Pagination";
+} from 'react-icons/fa'
+import { MdDeleteForever, MdVerified, MdDoNotDisturb } from 'react-icons/md'
+import './UserTable.scss'
+import ModalDelete from '../ModalDelete/index'
+import ModalBasic from '../ModalBasic/index'
+import Pagination from '../../atoms/Pagination'
 
 const UserTable = () => {
-  const itemsPerPage = 6;
-  const [users, setUsers] = useState([]);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [selectedUserId, setSelectedUserId] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sortDirection, setSortDirection] = useState("desc");
+  const itemsPerPage = 6
+  const [users, setUsers] = useState([])
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [selectedUserId, setSelectedUserId] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [sortDirection, setSortDirection] = useState('desc')
+  const [selectedUser, setSelectedUser] = useState(null)
+  const [modalOpen, setModalOpen] = useState(false)
 
-   const fetchData = async () => {
+  const handleEdit = (user) => {
+    setSelectedUser(user)
+    setModalOpen(true)
+  }
+
+  const handleSave = async (event) => {
+    event.preventDefault()
+
     try {
-      const token = localStorage.getItem('token');
-      const response = await fetch("http://localhost:8080/api/user", {
+      const response = await fetch(
+        `http://localhost:8080/api/user/${selectedUser._id}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(selectedUser),
+        },
+      )
+      if (response.ok) {
+        // Assuming fetchData is a function to refresh the user list
+        fetchData()
+        setModalOpen(false)
+      } else {
+        // Handle errors
+        console.error('Failed to update user')
+      }
+    } catch (error) {
+      console.error('Error updating user:', error)
+    }
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    // For boolean fields, we need to convert the string value to a boolean
+    const newValue =
+      name === 'isAdmin' || name === 'verified' ? value === 'true' : value
+    // For the dateOfBirth field, we need to ensure the value is a Date object
+    const finalValue = name === 'dateOfBirth' ? new Date(value) : newValue
+    setSelectedUser((prevUser) => ({
+      ...prevUser,
+      [name]: finalValue,
+    }))
+  }
+
+  const fetchData = async () => {
+    try {
+      const token = localStorage.getItem('token')
+      const response = await fetch('http://localhost:8080/api/user', {
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
+          Authorization: `Bearer ${token}`,
+        },
+      })
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error! status: ${response.status}`)
       }
-      
-      const data = await response.json();
+
+      const data = await response.json()
       // Sorting data by createdAt field
       const sortedData = data.slice().sort((a, b) => {
-        const dateA = new Date(a.createdAt);
-        const dateB = new Date(b.createdAt);
-        return dateB - dateA;
-      });
-      setUsers(sortedData);
+        const dateA = new Date(a.createdAt)
+        const dateB = new Date(b.createdAt)
+        return dateB - dateA
+      })
+      setUsers(sortedData)
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const handleDelete = async () => {
-    console.log('Deleting user with ID:', selectedUserId);
+    console.log('Deleting user with ID:', selectedUserId)
     try {
       await fetch(`http://localhost:8080/api/user/${selectedUserId}`, {
-        method: "DELETE",
-      });
-      fetchData();
-      setShowDeleteModal(false);
+        method: 'DELETE',
+      })
+      fetchData()
+      setShowDeleteModal(false)
     } catch (error) {
-      console.error("Error deleting user:", error);
+      console.error('Error deleting user:', error)
     }
-  };
+  }
 
   const handleSort = () => {
     const sortedData = [...users].sort((a, b) => {
-      const dateA = new Date(a.createdAt);
-      const dateB = new Date(b.createdAt);
-  
-      if (sortDirection === "desc") {
-        return dateB - dateA;
+      const dateA = new Date(a.createdAt)
+      const dateB = new Date(b.createdAt)
+
+      if (sortDirection === 'desc') {
+        return dateB - dateA
       } else {
-        return dateA - dateB;
+        return dateA - dateB
       }
-    });
-    setUsers(sortedData.reverse());
-    setSortDirection(sortDirection === "asc" ? "desc" : "asc");
-  };
+    })
+    setUsers(sortedData.reverse())
+    setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
+  }
 
   const handleSearch = (e) => {
-    const term = e.target.value;
-    setSearchTerm(term);
-    setCurrentPage(1); // Reset to the first page when a new search is performed
-  };
+    const term = e.target.value
+    setSearchTerm(term)
+    setCurrentPage(1) // Reset to the first page when a new search is performed
+  }
 
   const filteredUsers = users.filter((user) =>
-    user.username.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+    user.username.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentUsers = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentUsers = filteredUsers.slice(indexOfFirstItem, indexOfLastItem)
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
   const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleString(); // Adjust the formatting as needed
-  };
+    const date = new Date(dateString)
+    return date.toLocaleString() // Adjust the formatting as needed
+  }
 
   return (
     <main className="main-table">
@@ -115,11 +163,11 @@ const UserTable = () => {
               />
             </div>
             <button className="sort" onClick={handleSort}>
-              {sortDirection === "asc" ? (
+              {sortDirection === 'asc' ? (
                 <FaSortAmountUp />
               ) : (
                 <FaSortAmountDown />
-              )}{" "}
+              )}{' '}
               Sort
             </button>
           </div>
@@ -129,11 +177,12 @@ const UserTable = () => {
           <thead>
             <tr>
               <th>ID</th>
-              <th>Name</th>
+              <th>Username</th>
               <th>Email Address</th>
               <th>Date Created</th>
               <th>Date Updated</th>
               <th>Role</th>
+              <th>Verified</th>
               <th>Action</th>
             </tr>
           </thead>
@@ -142,19 +191,27 @@ const UserTable = () => {
               <tr key={user._id}>
                 <td>{user._id}</td>
                 <td>{user.username}</td>
-                <td>{user.email}</td>   
+                <td>{user.email}</td>
                 <td>{formatDate(user.createdAt)}</td>
                 <td>{formatDate(user.updatedAt)}</td>
-                <td>{user.isAdmin ? "Admin" : "Regular"}</td>
+                <td>{user.isAdmin ? 'Admin' : 'Regular'}</td>
+                <td style={{ textAlign: 'center', fontSize: '24px' }}>
+                  {user.verified ? (
+                    <MdVerified style={{ color: 'green' }} />
+                  ) : (
+                    <MdDoNotDisturb style={{ color: 'red' }} />
+                  )}
+                </td>
+
                 <td>
-                  <button className="editIcon">
+                  <button className="editIcon" onClick={() => handleEdit(user)}>
                     <FaEdit />
                   </button>
                   <button
                     className="deleteIcon"
                     onClick={() => {
-                      setSelectedUserId(user._id);
-                      setShowDeleteModal(true);
+                      setSelectedUserId(user._id)
+                      setShowDeleteModal(true)
                     }}
                   >
                     <MdDeleteForever />
@@ -170,7 +227,6 @@ const UserTable = () => {
           totalPages={Math.ceil(filteredUsers.length / itemsPerPage)}
           onPageChange={paginate}
         />
-
       </div>
 
       <ModalDelete
@@ -181,8 +237,82 @@ const UserTable = () => {
       >
         Are you sure you want to delete the user with ID:{selectedUserId}?
       </ModalDelete>
-    </main>
-  );
-};
 
-export default UserTable;
+      <ModalBasic
+        title={`Editing ${selectedUser?.username || 'User'}`}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+      >
+        <form onSubmit={handleSave}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              className='usernameEdit'
+              id="username"
+              name="username"
+              defaultValue={selectedUser?.username}
+              onChange={handleInputChange}
+              minLength="4"
+              maxLength="20"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              defaultValue={selectedUser?.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              defaultValue={selectedUser?.password}
+              onChange={handleInputChange}
+              minLength="8"
+              maxLength="64"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="isAdmin">Admin:</label>
+            <select
+              id="isAdmin"
+              name="isAdmin"
+              defaultValue={String(selectedUser?.isAdmin)}
+              onChange={handleInputChange}
+            >
+              <option value="false">Regular</option>
+              <option value="true">Admin</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="verified">Verified:</label>
+            <select
+              id="verified"
+              name="verified"
+              defaultValue={String(selectedUser?.verified)}
+              onChange={handleInputChange}
+            >
+              <option value="false">No</option>
+              <option value="true">Yes</option>
+            </select>
+          </div>
+          <button type="submit" className="saveButton">
+            Save
+          </button>
+        </form>
+      </ModalBasic>
+    </main>
+  )
+}
+
+export default UserTable
