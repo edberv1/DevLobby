151da72b0d8045facf336295c7820450f6edcaba
Shpat Spahiu <shpat.spahiu@proton.me>
2024-04-01 00:20:17 +0200

    Improve diaryForm

--- src/components/molecules/DiaryForm/index.jsx
+++ src/components/molecules/DiaryForm/index.jsx
@@ -8,21 +8,31 @@ const DiaryFrom = () => {
       <img src={ProfileGirl} alt='Profile' className='profile-img' />
 
       <div className='inputContainer'>
-        {/* <textarea /> */}
-        {/*     textarea {
-      border: none;
-      background-color: transparent;
-      resize: none;
-      outline: none;
-    }
-    textarea {
-      color: white;
-      padding: 1rem;
-      width: 90%;
-      background-color: #28343e;
-      border-radius: 12px;
-      margin-bottom: 2rem;
-    } */}
+        <textarea name='postContent' 
+        placeholder="What's going on today dev?"/>
+
+        <div className='actionContainer'>
+          <div className='addPhotoBtn'>
+            <svg
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+              <path d='M15 8h.01' />
+              <path d='M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z' />
+              <path d='M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5' />
+              <path d='M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3' />
+            </svg>
+          </div>
+
+          <div className="postBtn">Post</div>
+        </div>
       </div>
     </div>
   )
