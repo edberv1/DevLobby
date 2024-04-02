e3f78d19e54a4f348a5d5d399e12be09bc7fa36a
Shpat Spahiu <shpat.spahiu@proton.me>
2024-04-02 10:08:54 +0200

    Improve responsiveness

--- src/components/molecules/DiaryCard/index.jsx
+++ src/components/molecules/DiaryCard/index.jsx
@@ -18,6 +18,23 @@ const DiaryCard = ({
       <div className='diaryCard'>
         <div className='left'>
           <img src={ProfileGirl} alt='Profile' className='profile-img' />
+          <div className='optionsBtnMobile'>
+            <svg
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+              <path d='M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+              <path d='M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+              <path d='M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+            </svg>
+          </div>
         </div>
         <div className='middle'>
           <div className='cardHeader'>
